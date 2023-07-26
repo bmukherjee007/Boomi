00181c74f1f22b62b0270ac49abc691fe0c882d4
bmukherjee007 <140538322+bmukherjee007@users.noreply.github.com>
2023-07-26 13:26:41 +0530

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -24,12 +24,11 @@ wget -P /usr/local/boomi https://platform.boomi.com/atom/atom_install64.sh && ch
 if [ $1 = "Token" ]
 then
  echo "Install Atom using Token"
- echo "Print Varaibles $1 $2 $3 $4 $5 $6"
  echo "boomiPasswordORboomiAPIToken: $boomiPasswordORboomiAPIToken"
  echo "atomName: $atomName"
  echo "boomiAccountID: ${boomiAccountID}"
  sudo /usr/local/boomi/atom_install64.sh -q console -VinstallToken="${boomiPasswordORboomiAPIToken}" -VatomName="${atomName}" -VaccountId="${boomiAccountID}" -dir "/opt/boomi/"        
 else
  echo "Install Atom using Password"
- sudo /usr/local/boomi/atom_install64.sh -q console -Vusername="$2" -Vpassword="$3" -VatomName="$5" -VaccountId="$4" -dir "/opt/boomi/"        
+ sudo /usr/local/boomi/atom_install64.sh -q console -Vusername="${boomiUserEmailID}" -Vpassword="${boomiPasswordORboomiAPIToken}" -VatomName="${atomName}" -VaccountId="${boomiAccountID}" -dir "/opt/boomi/"        
 fi
