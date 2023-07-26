7341dfba65ba39d88f98de5f2677fb1ccd76dd12
bmukherjee007 <140538322+bmukherjee007@users.noreply.github.com>
2023-07-26 10:32:57 +0530

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -9,8 +9,8 @@
 #exec 1>/var/log/configure-bastion.log 2>&1
 
 #sudo yum -y install java-1.8.0-openjdk
-sudo apt-get update
-sudo apt install openjdk-8-jre-headless -y
+#sudo apt-get update
+#sudo apt install openjdk-8-jre-headless -y
 
 #sudo yum -y install wget
 sudo apt-get update
@@ -22,11 +22,11 @@ AuthType=$(echo "${boomiAuthenticationType}" | xargs)
 AuthType=$(echo "$AuthType" | tr '[:lower:]' '[:upper:]')
 if [ $AuthType = TOKEN ]
 then
- TOKEN="$(gsutil cat gs://${deploymentName}-bucket/token.txt | head -1 | base64 --decode)"
- gsutil rm gs://${deploymentName}-bucket/token.txt
+ #TOKEN="$(gsutil cat gs://${deploymentName}-bucket/token.txt | head -1 | base64 --decode)"
+ #gsutil rm gs://${deploymentName}-bucket/token.txt
  sudo /usr/local/boomi/atom_install64.sh -q console -VinstallToken=$TOKEN -VatomName=${atomName} -VaccountId=${boomiAccountID} -dir "/opt/boomi/"        
 else
- Password="$(gsutil cat gs://${deploymentName}-bucket/token.txt | head -1 | base64 --decode)"
- gsutil rm gs://${deploymentName}-bucket/token.txt
+ #Password="$(gsutil cat gs://${deploymentName}-bucket/token.txt | head -1 | base64 --decode)"
+ #gsutil rm gs://${deploymentName}-bucket/token.txt
  sudo /usr/local/boomi/atom_install64.sh -q console -Vusername=${boomiUserEmailID} -Vpassword=$Password -VatomName=${atomName} -VaccountId=${boomiAccountID} -dir "/opt/boomi/"        
 fi
