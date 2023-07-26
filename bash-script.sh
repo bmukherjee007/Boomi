e3bf8c8230161fb291821b7c77b5b961e2b57723
bmukherjee007 <140538322+bmukherjee007@users.noreply.github.com>
2023-07-26 11:28:10 +0530

    Update bash-script.sh

--- bash-script.sh
+++ bash-script.sh
@@ -25,15 +25,15 @@ sudo apt-get install wget
 wget -P /usr/local/boomi https://platform.boomi.com/atom/atom_install64.sh && chmod +x /usr/local/boomi/atom_install64.sh
 #chmod 777 /usr/local/boomi/atom_install64.sh
 
-AuthType=$(echo "${boomiAuthenticationType}" | xargs)
-AuthType=$(echo "$AuthType" | tr '[:lower:]' '[:upper:]')
-if [ $AuthType = TOKEN ]
-then
+#AuthType=$(echo "${boomiAuthenticationType}" | xargs)
+#AuthType=$(echo "$AuthType" | tr '[:lower:]' '[:upper:]')
+#if [ $AuthType = TOKEN ]
+#then
  #TOKEN="$(gsutil cat gs://${deploymentName}-bucket/token.txt | head -1 | base64 --decode)"
  #gsutil rm gs://${deploymentName}-bucket/token.txt
- sudo /usr/local/boomi/atom_install64.sh -q console -VinstallToken=${boomiPasswordORboomiAPIToken} -VatomName=${atomName} -VaccountId=${boomiAccountID} -dir "/opt/boomi/"        
-else
+ sudo /usr/local/boomi/atom_install64.sh -q console -VinstallToken="atom-2845082b-2167-4fe7-b9f1-f48a1be397e9" -VatomName="Test-Linux-Atom" -VaccountId="pwc-UWWBSP" -dir "/opt/boomi/"        
+#else
  #Password="$(gsutil cat gs://${deploymentName}-bucket/token.txt | head -1 | base64 --decode)"
  #gsutil rm gs://${deploymentName}-bucket/token.txt
- sudo /usr/local/boomi/atom_install64.sh -q console -Vusername=${boomiUserEmailID} -Vpassword=${boomiPasswordORboomiAPIToken} -VatomName=${atomName} -VaccountId=${boomiAccountID} -dir "/opt/boomi/"        
-fi
+# sudo /usr/local/boomi/atom_install64.sh -q console -Vusername=${boomiUserEmailID} -Vpassword=${boomiPasswordORboomiAPIToken} -VatomName=${atomName} -VaccountId=${boomiAccountID} -dir "/opt/boomi/"        
+#fi
